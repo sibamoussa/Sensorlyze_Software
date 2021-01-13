6d2c039abb5875058909dcc9107628288c2038b2
sibamoussa <40647960+sibamoussa@users.noreply.github.com>
2021-01-13 06:10:53 -0500

    Delete Main directory

--- Main/open_file.py
+++
@@ -1,8 +0,0 @@
-from import_modules import*
-def openFile(self):
-# temporary fix to show the filename
-    options = QFileDialog.Options()
-    options |= QFileDialog.DontUseNativeDialog
-    fileName, _ = QFileDialog.getOpenFileNames(self.window, "Import File", "",
-                                                  "All Files (*);;Data Files (*.asc);;Excel Files (*.xlsx);;Text Files (*.txt)", options=options)
-    return (fileName)
